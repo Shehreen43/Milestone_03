57eafd1073158641f3d26add2109754a12c61c57
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2025-01-01 21:10:26 +0500

    working...

--- nextjs-e-commerce/app/(store)/search/page.tsx
+++ nextjs-e-commerce/app/(store)/search/page.tsx
@@ -1,12 +1,49 @@
+import ProductGrid from "@/components/ProductGrid";
+import { Product } from "@/sanity.types";
+import { searchProductByName } from "@/sanity/lib/product/searchProductByName";
 
+async function SearchPage({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) {
+  const { query } = searchParams;
+  const response = await searchProductByName(query);
 
-async function SearchPage({searchParams,}: {searchParams: {query:string}}) {
-    const {query} = await searchParams;
+  // Check if response is an array or an object with 'data'
+  let products: Product[] = [];
+
+  if (Array.isArray(response)) {
+    products = response; // If it's an array, use it directly
+  } else if (response.data) {
+    products = response.data; // If it's an object with 'data', use that
+  }
+
+  if (products.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+          <h1 className="text-3xl font-bold mb-6 text-center">
+            No products found for: <span className="text-red-500">{query}</span>
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-          search for {query}
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search Results for: <span className="text-blue-500">{query}</span>
+        </h1>
+        <ProductGrid products={products} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default SearchPage;
--- nextjs-e-commerce/sanity/lib/product/getAllProducts.ts
+++ nextjs-e-commerce/sanity/lib/product/getAllProducts.ts
@@ -2,12 +2,7 @@ import { defineQuery } from "next-sanity";
 import { sanityFetch } from "../live";
 
 export const getAllProducts = async () => {
-  // Define Query
-  const ALL_PRODUCTS_QUERY = defineQuery(
-    `
-      *[_type == "product"] | order(name asc)
-    `
-  );
+  const ALL_PRODUCTS_QUERY = defineQuery(`*[_type == "product"] | order(name asc)`);
 
   try {
     // Use sanityFetch to send the query
