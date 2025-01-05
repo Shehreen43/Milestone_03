b368b21385cc927bd0011ed3a862b9fdf460e630
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2025-01-05 11:00:49 +0500

    ....

--- nextjs-e-commerce/actions/createCheckoutSession.ts
+++ nextjs-e-commerce/actions/createCheckoutSession.ts
@@ -1,4 +1,4 @@
-"use server";
+ "use server";
 
 import { BasketItem } from "@/store/store";
 
@@ -24,6 +24,31 @@ export async function createCheckoutSession(
         if (itemWithoutPrice.length > 0) {
             throw new Error("Some item do not have a price")
         }
+
+         //Search for existing customer by email
+         const customers = await stripe.customers.list({
+            email: metadata.customerEmail,
+            limit: 1,
+         });
+
+         let customerId: string | undefined;
+         if (customers.data.length > 0) {
+            customerId = customers.data[0].id;
+         }
+    
+    const session = await stripe.checkout.sessions.create({
+        customer: customerId,
+        customer_creation: customerId ? undefined : "always",
+        customer_email: !customerId ? metadata.customerEmail : undefined,
+        metadata,
+        mode: "payment",
+        allow_promotion_codes: true,
+        success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_PUBLIC_URL}/success?session_id={CHECKOUT_SESSION_ID}$orderNumber=${metadata.orderNumber}`,
+        cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+    
+    
+    })
+
     } catch (error) {
         console.log("Error creating checkout session");
         throw error;
